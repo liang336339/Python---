c27c94033833c378fb405a2f2950c5146346afdc
liang336339 <83640611+liang336339@users.noreply.github.com>
2021-06-02 10:34:10 +0800

    Add files via upload
    It is ok ,can readteh data of 2*1000*100

--- MatfileRead.py
+++ MatfileRead.py
@@ -1,7 +1,7 @@
 def ReadData(fileName, IsShow):
     import scipy.io as sio
     import numpy as np
-    print('正在读取数据中 》》》')
+    print('Reading data ...')
     X = sio.loadmat(fileName)
     # test_data
     # train_data
@@ -10,11 +10,13 @@ def ReadData(fileName, IsShow):
     # Data['train_data'][1][1][0]  行 列 另一组数据
     # Data['train_label'][1]
     # 时间信息 0-5s 为运动想象的时间
+    # 选择通道为C3 和 C4
     C3 = X['train_data'][27, 0:1000,:]
     C4 = X['train_data'][33, 0:1000,:]
-    X['train_data'] = ([C3]
-    [C4])
-    print(type(C3))
+    X['train_data'] = np.array([C3[:,:],C4[:,:]])
+    C3 = X['test_data'][27, 0:1000,:]
+    C4 = X['test_data'][33, 0:1000,:]
+    X['test_data'] = np.array([C3[:,:],C4[:,:]])
     if(IsShow == 1):
         print(type(X))
     return X
